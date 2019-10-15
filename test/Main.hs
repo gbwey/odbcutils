73374315f733492155b3bd14ff9fa3e1268c8881
grant weyburne <gbwey9@gmail.com>
2019-10-15 15:47:27 -0400

    replace easytest with tasty-hunit

--- test/Main.hs
+++ test/Main.hs
@@ -5,15 +5,15 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 import OdbcUtils
-import EasyTest
-import Control.Lens
+import Test.Tasty
+import Test.Tasty.HUnit
+--import Control.Lens
 import Control.Lens.Error
 import Database.ODBC.SQLServer
-import Data.Maybe
 
 main :: IO ()
-main =
-  run $ tests [
-        scope "dir in file" $ expectEq (Success (IntValue 13)) ([RData [] [[IntValue 13]]] ^&? row1col1)
+main = defaultMain $ testGroup "OdbcUtils"
+     [
+        testCase "dir in file" $ (@?=) (Success (IntValue 13)) ([RData [] [[IntValue 13]]] ^&? row1col1)
      ]
 
